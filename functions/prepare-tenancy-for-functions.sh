90883ea541921fde0769e77cd8452fc7ed11d499
Lucas Jellema <lucasjellema@gmail.com>
2019-12-23 09:59:01 +0000

    Leave network resources to Network Quickstart wizard

--- functions/prepare-tenancy-for-functions.sh
+++ functions/prepare-tenancy-for-functions.sh
@@ -5,20 +5,23 @@
 
 FN_COMPARTMENT_NAME=functions-compartment
 FN_DEVS_GROUP=FN_DEVELOPERS
-FN_VCN_NAME=fn-vcn
+FN_VCN_NAME=VCN-FN
+FN_SUBNET_NAME='Public Subnet-VCN-FN'
 FN_IGW_NAME=internet-gateway-fn
 FN_MANAGE_REPOS_POLICY=fn-repos-management
 FN_MANAGE_APP_POLICY=fn-app-management
 FN_GROUP_USE_VCN_POLICY=fn-group-use-network-family
 FN_FAAS_USE_VCN_POLICY=fn-faas-use-network-family
 FN_FAAS_READ_REPOS_POLICY=fn-faas-read-repos
+FN_FUNCTION_APP=lab-app
 
 oci() { docker run --rm --mount type=bind,source=$HOME/.oci,target=/root/.oci stephenpearson/oci-cli:latest "$@"; }
+export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
 
 ROOT_COMPARTMENT_OCID=$1
 
 # create a dedicated compartment for Functions
-CREATE_FN_COMP="true"
+CREATE_FN_COMP="false"
 if [ $CREATE_FN_COMP = "true" ]
 then
   echo "creating $FN_COMPARTMENT_NAME"
@@ -56,13 +59,13 @@ get_vcn_id()
 }
 
 
-CREATE_VCN="true"
+CREATE_VCN="false"
 if [ $CREATE_VCN = "true" ]
 then
         # create a virtual cloud network
         echo "creating virtual cloud network"
         # https://docs.cloud.oracle.com/iaas/Content/ContEng/Concepts/contengnetworkconfigexample.htm
-        oci network vcn create --compartment-id $FN_COMPARTMENT_OCID --cidr-block '172.16.1.0/24'  --display-name $FN_VCN_NAME  --dns-label 'fn' 
+        oci network vcn create --compartment-id $FN_COMPARTMENT_OCID --cidr-block '10.0.0.0/16'  --display-name $FN_VCN_NAME  --dns-label 'fn' 
         echo "created a new VCN: $FN_VCN"
 else  
   echo "skipping VCN creation"
@@ -70,8 +73,37 @@ fi
 FN_VCN_OCID=`get_vcn_id $FN_VCN_NAME $FN_COMPARTMENT_OCID` 
 echo "OCID for $FN_VCN_NAME= $FN_VCN_OCID"
 
+# invoke this function with three arguments; 
+# 1. name of subnet
+# 2. OCID of VCN
+# 3. OCID of compartment
+get_subnet_id()
+{
+  SUBNET_DISPLAY_NAME_VAR=$1     
+  FN_VCN_OCID_VAR=$2
+  FN_COMPARTMENT_OCID_VAR=$3     
+  vcns=$(oci network subnet list  --compartment-id $FN_COMPARTMENT_OCID_VAR --vcn-id $FN_VCN_OCID_VAR --all)
+  local THE_SUBNET_ID=$(echo $vcns | jq -r --arg display_name "$1" '.data | map(select(."display-name" == $display_name)) | .[0] | .id')
+  # this line provides the output from this function; this output can be captured in a variable by the caller using VAR=`function param1`  
+  echo "$THE_SUBNET_ID"
+}
 
-CREATE_IGW="true"
+
+
+
+CREATE_SUBNET="false"
+if [ $CREATE_SUBNET = "true" ]
+then
+        # create a virtual cloud network
+        echo "creating subnet"
+        oci network subnet create --compartment-id $FN_COMPARTMENT_OCID --vcn-id $FN_VCN_OCID --cidr-block '10.0.0.0/24'  --display-name $FN_SUBNET_NAME  --dns-label fn-subnet 
+else  
+  echo "skipping VCN creation"
+fi
+FN_SUBNET_OCID=`get_subnet_id $FN_SUBNET_NAME $FN_VCN_OCID $FN_COMPARTMENT_OCID` 
+echo "OCID for $FN_SUBNET_NAME= $FN_SUBNET_OCID"
+
+CREATE_IGW="false"
 if [ $CREATE_IGW = "true" ]
 then
   echo "creating Internet Gateway in the VCN"
@@ -102,3 +134,5 @@ oci iam policy create  --name $FN_FAAS_USE_VCN_POLICY --compartment-id $ROOT_COM
 echo creating policy $FN_FAAS_READ_REPOS_POLICY
 oci iam policy create  --name $FN_FAAS_READ_REPOS_POLICY --compartment-id $ROOT_COMPARTMENT_OCID  --statements "[ \"Allow service FaaS to read repos in tenancy\"]"  --description "Create a Policy to Give the Oracle Functions Service Access to Repositories in Oracle Cloud Infrastructure Registry"
 
+# creating Function App
+oci fn application create --display-name $FN_FUNCTION_APP --compartment-id $FN_COMPARTMENT_OCID --subnet-ids "[\"$FN_SUBNET_OCID\"]"
